9149a6f3f4a3e078b0eb6dc0eb535ddce1647df4
nishant <nishantsubedi1996@gmail.com>
2018-04-06 20:16:52 +0545

    Added add-page for pages

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+* GET pages index
+*/
 router.get('/', function(req, res){
     res.render('index', {
         title: 'Admin Area' 
     });
 });
+
+/*
+* GET add page
+*/
+router.get('/add-page', function(req, res){
+   var  title = "";
+   var slug = "";
+   var content = "";
+
+   res.render('admin/add_page', {
+       title: title,
+       slug: slug,
+       content: content
+   })
+});
+
+
 
 // Exports
 module.exports = router;
